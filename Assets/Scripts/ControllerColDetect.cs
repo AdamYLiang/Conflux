205e74751ea514be654069c4f3508e27b1d3aa89
AdamYLiang <ayl292@nyu.edu>
2017-02-03 20:17:59 -0500

    Basic hookup with CubePuzzle, able to click activate it

---
+++ Assets/Scripts/ControllerColDetect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerColDetect : MonoBehaviour {
+
+    void OnTriggerStay(Collider activator)
+    {
+        if(activator.tag == "CubePuzzle")
+        {
+           // Debug.Log("ITS PUZZLE TIME BOYZ");
+            transform.parent.GetComponent<RotationController>().isTouchingPuzzle = true;
+            transform.parent.GetComponent<RotationController>().currentPuzzle = activator.transform.root.gameObject;
+        }
+    }
+
+    //void OnTriggerExit(Collider activator)
+    //{
+    //    if(activator.tag == "CubePuzzle")
+    //    {
+    //        Debug.Log("Left the puzzle how dare you");
+    //        transform.parent.GetComponent<RotationController>().isTouchingPuzzle = false;
+    //        activator.transform.root.gameObject.GetComponent<PuzzleManager>().play = false;
+    //        transform.parent.GetComponent<RotationController>().currentPuzzle = null;
+    //    }
+    //}
+}
---
+++ Assets/Scripts/RotationController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device controller;
+    public bool isTouchingPuzzle;
+    public GameObject currentPuzzle;
+
+    // Use this for initialization
+    void Start () {
+        isTouchingPuzzle = false;
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        controller = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if (isTouchingPuzzle)
+            {
+                currentPuzzle.GetComponent<PuzzleManager>().play = true;
+                currentPuzzle.transform.FindChild("CubePuzzle").gameObject.SetActive(false);
+            }
+            //Debug.Log("Trigger Pressed");
+            controller.TriggerHapticPulse(700);
+        }
+
+        if (controller.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if (isTouchingPuzzle)
+            {
+                currentPuzzle.GetComponent<PuzzleManager>().play = false;
+                currentPuzzle.transform.FindChild("CubePuzzle").gameObject.SetActive(true);
+                isTouchingPuzzle = false;
+                currentPuzzle = null;
+            }
+        }
+
+    }
+
+}
