5f138d71fe6afc365864f7db990f14fc97272e2b
Calvin So <ccs403@nyu.edu>
2017-02-14 11:58:51 -0500

    Backup before merge

--- Assets/Scripts/CubePuzzleScripts/TileUpdater.cs
+++ Assets/Scripts/CubePuzzleScripts/TileUpdater.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class TileUpdater : MonoBehaviour {
 
-    public CubeSpawner puzzleEditor; 
+    public CubeSpawner puzzleEditor;
+
+    List<GameObject> tiles;
+
+    public int tileType;
+    private GameObject originalTileType;
+    
+    public FaceInfo.WallNumber wallNumber = FaceInfo.WallNumber.Zero;
 
 	// Use this for initialization
 	void Start () {
-		
+        puzzleEditor = transform.root.GetComponent<CubeSpawner>();
+        tiles = puzzleEditor.Tiles;
+        originalTileType = tiles[tileType];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+        //If we have changed the tile type
+        if(tiles[tileType] != originalTileType)
+        {
+            Replace();
+        }
+
 	}
+
+    //Replace ourselves with the correct tile type.
+    public void Replace()
+    {
+        GameObject temp = Instantiate(tiles[tileType], transform.position, transform.rotation, transform.parent);
+
+        Destroy(gameObject);
+    }
+
+    //Copy the values onto the new object when we spawn it.
+    public void CopyValues(TileUpdater tile)
+    {
+        tiles = tile.tiles;
+        wallNumber = tile.wallNumber;
+    }
+
 }
