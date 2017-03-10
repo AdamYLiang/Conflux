927ad88131fb56e9e36b371f72160a8c2c60ca2b
ayl292 <ayl292@nyu.edu>
2017-03-09 21:22:58 -0500

    major updates to door and room code, added endings for first documentation video, fixed walls to be texture instead of mesh

---
+++ Assets/RoomInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomInfo : MonoBehaviour {
+
+
+    public List<GameObject> doors;
+    private List<DoorInfo> doorInfo;
+
+	// Use this for initialization
+	void Start () {
+		
+        for(int i = 0; i < doors.Count - 1; i++)
+        {
+            DoorInfo temp = new DoorInfo(doors[i]);
+            doorInfo.Add(temp);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public class DoorInfo
+    {
+        bool open = false;
+        GameObject door = null;
+
+        public DoorInfo(GameObject obj)
+        {
+            door = obj;
+        }
+    }
+
+
+}
---
+++ Assets/Tiler.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Note: Modified and updated version of:
+//          http://ericeastwood.com/blog/20/texture-tiling-based-on-object-sizescale-in-unity
+
+//Only run in edit mode.
+[ExecuteInEditMode]
+public class Tiler : MonoBehaviour {
+
+    //Get and store texture from renderer
+    public Material mat;
+    private Texture texture;
+
+    //Our desired texture control. Change this to affect ______
+    public float textureToMeshZ = 2f;
+
+    //Just saves whether or not we have changed the corresponding public variable
+    Vector3 prevScale = Vector3.one;
+
+    //Just saves whether or not we have changed the public variable
+    float prevTextureToMeshZ = -1f;
+
+    //Square tiling
+    public bool isSquare = false;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<Renderer>().material;
+
+        texture = mat.mainTexture;
+
+        //Save our scale to the current scale
+        if (isSquare)
+        {
+            //Only use one value to maintain a "cube" ratio
+            prevScale = new Vector3(gameObject.transform.lossyScale.x, gameObject.transform.lossyScale.z, gameObject.transform.lossyScale.z);
+            
+        }
+        else
+        {
+            prevScale = gameObject.transform.lossyScale;
+        }
+
+        //Set to public variables
+        prevTextureToMeshZ = textureToMeshZ;
+
+        UpdateTiling();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //If either our scale has changed or 
+        if(gameObject.transform.lossyScale != prevScale || 
+            !Mathf.Approximately(textureToMeshZ, prevTextureToMeshZ))
+        {
+            UpdateTiling();
+        }
+
+        //Save our scale to the current scale
+        if (isSquare)
+        {
+            //Only use one value to maintain a "cube" ratio
+            prevScale = new Vector3(gameObject.transform.lossyScale.x, gameObject.transform.lossyScale.z, gameObject.transform.lossyScale.z);
+        }
+        else
+        {
+            prevScale = gameObject.transform.lossyScale;
+        }
+        prevTextureToMeshZ = textureToMeshZ;
+	}
+
+    [ContextMenu("UpdateTiling")]
+    void UpdateTiling()
+    {
+        //Standard plane is 10 x 10
+        float planeSizeX = 10f;
+        float planeSizeZ = 10f;
+
+        //Calculate the texture-to-mesh width based on the ratio given by user-set texture-to-mesh height.
+        float textureToMeshX = ((float)texture.width / texture.height) * textureToMeshZ;
+
+        //Set the values.
+        gameObject.GetComponent<Renderer>().material.mainTextureScale =
+            new Vector2(planeSizeX * gameObject.transform.lossyScale.x / textureToMeshZ,   
+                planeSizeZ * gameObject.transform.lossyScale.z / textureToMeshZ);
+    }
+}
